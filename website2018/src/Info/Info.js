0aace826b4b39864dcdef8624b90e5f28283d2db
Jason Manuel <jama.indo@hotmail.com>
2019-11-19 22:19:48 -0700

    Remove the terminal feature
    Although the terminal was an interesting interaction pattern to have, it was too confusing and inefficient compared to the info cards. It did not provide enough value; thus, it was removed.

--- website2018/src/Info/Info.js
+++ website2018/src/Info/Info.js
@@ -1,68 +1,22 @@
 import React, { Component } from 'react';
-import { Divider, Button, Grid } from 'semantic-ui-react';
+import { Divider } from 'semantic-ui-react';
 
-import Term from './Term.js';
 import InfoCards from './InfoCards.js';
 import './Info.css';
 
 class Info extends Component {
 
-    constructor(props) {
-    	super(props);
-		this.state = {terminal: true};
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-
-	handleClick = () => {
-		if(this.state.terminal) {
-			this.setState({terminal: false})
-		} else {
-			this.setState({terminal: true})
-		}
-		this.forceUpdate();
-		console.log("button clicked");
-	}
-
     render() {
 
-		if(this.state.terminal) {
 			return (
 				<div id="info">
-				<Divider horizontal id="title">Info</Divider>
-				<InfoCards/>
-				<Grid>
-					<Grid.Column textAlign="center">
-						<Button.Group>
-							<Button positive>Cards</Button>
-							<Button.Or />
-							<Button onClick={this.handleClick}>Terminal</Button>
-						</Button.Group>
-					</Grid.Column>
-				</Grid>
-			</div>		
+					<Divider horizontal id="title">Info</Divider>
+					<InfoCards/>
+				</div>
 			);
-		} else {
-			return (		
-				<div id="info">
-				<Divider horizontal id="title">Info</Divider>
-				<Term/>
-				<Grid>
-					<Grid.Column textAlign="center">
-						<Button.Group>
-							<Button onClick={this.handleClick}>Cards</Button>
-							<Button.Or />
-							<Button positive>Terminal</Button>							
-						</Button.Group>
-					</Grid.Column>
-				</Grid>
-			</div>
-			);
-		}
 
 	}
-	
-    onClose = (modal) => () => this.setState({[modal]: false});
+
 }
 
 export default Info;
