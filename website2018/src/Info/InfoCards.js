97fa9f500fdf3ff3b4e9ebe8ea9bf49c6ab830f1
Jason Manuel <jama.indo@hotmail.com>
2020-02-28 16:24:18 -0700

    Make 'view your current points' URL a real link
    Members should not have to copy and paste a URL to fill out a form. Now, they can click on the link to access the form.
    
    The disadvantage of this change is that we dangerouslySetInnerHTML.

--- website2018/src/Info/InfoCards.js
+++ website2018/src/Info/InfoCards.js
@@ -14,15 +14,17 @@ class InfoCards extends Component {
 						<Card.Content>
 							<Card.Header>{title}</Card.Header>
 							<br/>
-							<Modal key={i} trigger={<Button>More Information</Button>} header={title} content={content.map((content) => <div>{content}</div>)}></Modal>
+							<Modal key={i} trigger={<Button>More Information</Button>} header={title} content={content.map((content) => {
+								content = {__html: content};
+								/* QUESTION: Is there a better way to display links in content.json? */
+								return <div dangerouslySetInnerHTML={content}></div>;
+							})}></Modal>
 						</Card.Content>
 					</Card>
-
-					)}
+				)}
 			</div>
 		);
 	}
-
 }
 
 export default InfoCards;
