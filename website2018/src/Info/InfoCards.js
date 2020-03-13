56fd4adc26e85963f75ab79dbd35fe17b9e3e16a
Jason Manuel <jama.indo@hotmail.com>
2020-03-13 14:26:15 -0700

    Move InfoCards content to a JSX file
    This allows us to embed markup within the content instead of using dangerouslySetInnerHTML.

--- website2018/src/Info/InfoCards.js
+++ website2018/src/Info/InfoCards.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-import content from '../assets/content.json';
+import content from './content.js';
 import {Card, Button, Modal} from 'semantic-ui-react';
 import './InfoCards.css'
 
 class InfoCards extends Component {
 
 	render() {
-		const items = content.content;
 		return (
 			<div id="cards">
-				{items.map(({title, content, i}) =>
+				{content.map(({title, content}) =>
 					<Card key={title} className="Card">
 						<Card.Content>
 							<Card.Header>{title}</Card.Header>
 							<br/>
-							<Modal key={i} trigger={<Button>More Information</Button>} header={title} content={content.map((content) => {
-								content = {__html: content};
-								/* QUESTION: Is there a better way to display links in content.json? */
-								return <div dangerouslySetInnerHTML={content}></div>;
-							})}></Modal>
+							<Modal trigger={<Button>More Information</Button>} header={title} content={content}></Modal>
 						</Card.Content>
 					</Card>
 				)}
